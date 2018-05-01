10191a4067caa6b6e5bddf821813f9ee497d29b1
gitjunghyun <leeu4728@gmail.com>
2018-05-01 14:56:42 +0900

    초기 세팅

---
+++ src/main/java/com/goon/GoOnApplication.java
@@ -0,0 +1,12 @@
+package com.goon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoOnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoOnApplication.class, args);
+	}
+}
