b0dc50bffc5b600bc1994ecf2c038c1c67bdd2c7
DESKTOP-H0JJAU5\leeu <leeu4728@gmail.com>
2018-05-28 02:14:10 +0900

    lombok 삭제

--- src/main/java/com/goon/domain/Passenger.java
+++ src/main/java/com/goon/domain/Passenger.java
@@ -12,7 +12,6 @@ import lombok.Data;
 
 @Entity
 @Table(name="Passenger")
-@Data
 public class Passenger {
 
 	@Id
@@ -29,4 +28,66 @@ public class Passenger {
 	private String PsgTell;
 	private String PsgJoinDate = new SimpleDateFormat("yyyy/MM/dd HH:mm:ss").format(Calendar.getInstance().getTime());
 	private String PsgPicture;
+	
+	@Override
+	public String toString() {
+		return "Passenger [Id=" + Id + ", PsgName=" + PsgName + ", PsgEmail=" + PsgEmail + ", PsgPassword="
+				+ PsgPassword + ", PsgAuth=" + PsgAuth + ", PsgGender=" + PsgGender + ", PsgTell=" + PsgTell
+				+ ", PsgJoinDate=" + PsgJoinDate + ", PsgPicture=" + PsgPicture + "]";
+	}
+	
+	public Long getId() {
+		return Id;
+	}
+	public void setId(Long id) {
+		Id = id;
+	}
+	public String getPsgName() {
+		return PsgName;
+	}
+	public void setPsgName(String psgName) {
+		PsgName = psgName;
+	}
+	public String getPsgEmail() {
+		return PsgEmail;
+	}
+	public void setPsgEmail(String psgEmail) {
+		PsgEmail = psgEmail;
+	}
+	public String getPsgPassword() {
+		return PsgPassword;
+	}
+	public void setPsgPassword(String psgPassword) {
+		PsgPassword = psgPassword;
+	}
+	public int getPsgAuth() {
+		return PsgAuth;
+	}
+	public void setPsgAuth(int psgAuth) {
+		PsgAuth = psgAuth;
+	}
+	public String getPsgGender() {
+		return PsgGender;
+	}
+	public void setPsgGender(String psgGender) {
+		PsgGender = psgGender;
+	}
+	public String getPsgTell() {
+		return PsgTell;
+	}
+	public void setPsgTell(String psgTell) {
+		PsgTell = psgTell;
+	}
+	public String getPsgJoinDate() {
+		return PsgJoinDate;
+	}
+	public void setPsgJoinDate(String psgJoinDate) {
+		PsgJoinDate = psgJoinDate;
+	}
+	public String getPsgPicture() {
+		return PsgPicture;
+	}
+	public void setPsgPicture(String psgPicture) {
+		PsgPicture = psgPicture;
+	}
 }
