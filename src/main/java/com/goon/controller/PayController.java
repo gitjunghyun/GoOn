a1754069687b5d7544bd5905d9096ceaf1ecc956
gitjunghyun <leeu4728@gmail.com>
2018-06-16 23:20:36 +0900

    pay 기능 수정

--- src/main/java/com/goon/controller/PayController.java
+++ src/main/java/com/goon/controller/PayController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class PayController {
 
-	@GetMapping("/pay")
+	@GetMapping("/pay/kakaopay")
 	public String pay() {
 		return "/pay/kakaopay";
 	}
