0c4328fd52072c85d622ce1e53826a7f477b7e20
gitjunghyun <leeu4728@gmail.com>
2018-06-20 13:34:19 +0900

    180620 Push

--- src/main/java/com/goon/controller/MapController.java
+++ src/main/java/com/goon/controller/MapController.java
@@ -1,6 +1,6 @@
 package com.goon.controller;
 
-import org.springframework.stereotype.Controller;
+import org.springframework.stereotype.Controller;	
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -8,29 +8,29 @@ import org.springframework.web.bind.annotation.PostMapping;
 @Controller
 public class MapController {
 
-	// 구글 맵
-	@GetMapping("/map/google")
-	public String google() {
-		return "/map/google";
-	}
-
-	// 구글 맵
+	// 메인 지도 화면
 	@GetMapping("/map/main")
 	public String main() {
 		return "/map/main";
 	}
+	
+//	// test
+//	@GetMapping("/map/google")
+//	public String googleee() {
+//		return "/map/google";
+//	}
 
-	// test
-	@GetMapping("/map/test")
-	public String maptest() {
-		return "/map/test";
-	}
-
-	// 설정한 경로 표시
-	@PostMapping("/map/result")
-	public String mapresult(String start, String destination, Model model) {
-		model.addAttribute("start", start);
-		model.addAttribute("destination", destination);
-		return "/map/result";
-	}
+//	// test
+//	@GetMapping("/map/test")
+//	public String maptest() {
+//		return "/map/test";
+//	}
+//
+//	// 설정한 경로 표시
+//	@PostMapping("/map/result")
+//	public String mapresult(String start, String destination, Model model) {
+//		model.addAttribute("start", start);
+//		model.addAttribute("destination", destination);
+//		return "/map/result";
+//	}
 }
